123b8c8442858e76492080a22f553ec2c0a133bf
Nopi96912 <nopiwork@gmail.com>
2018-01-31 09:43:09 +0100

    User class basic implementation

--- src/main/java/com/codecool/adhoc/ticketportal/model/User.java
+++ src/main/java/com/codecool/adhoc/ticketportal/model/User.java
@@ -1,4 +1,88 @@
 package com.codecool.adhoc.ticketportal.model;
 
+import com.codecool.adhoc.ticketportal.model.enums.UserType;
+
 public class User {
+
+    private Long id;
+    private String userName;
+    private String email;
+    private String fullName;
+    private String password;
+    private String cardNumber;
+    private Cart cart;
+    private UserType userType;
+
+    public User() {
+    }
+
+    public User(String userName, String email, UserType userType) {
+        this.userName = userName;
+        this.email = email;
+        this.userType = userType;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
 }
