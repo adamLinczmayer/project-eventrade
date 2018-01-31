55412b3922d99de6889679f5d830d50cbe7b98fe
fruzsinaCsongor <csongor.fruzsina@gmail.com>
2018-01-31 09:38:35 +0100

    Cart class created

--- src/main/java/com/codecool/adhoc/ticketportal/model/Cart.java
+++ src/main/java/com/codecool/adhoc/ticketportal/model/Cart.java
@@ -1,4 +1,15 @@
 package com.codecool.adhoc.ticketportal.model;
 
 public class Cart {
+
+    private long id;
+
+    public Cart() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+
 }
