3b6c36742a8ad96ee26946b3a9448e71874beefb
Sz. N. Bálint <szalaynb93@gmail.com>
2018-01-31 09:30:36 +0100

    Location class ready

--- src/main/java/com/codecool/adhoc/ticketportal/model/Location.java
+++ src/main/java/com/codecool/adhoc/ticketportal/model/Location.java
@@ -1,4 +1,46 @@
 package com.codecool.adhoc.ticketportal.model;
 
 public class Location {
+
+    private long id;
+    private String name;
+    private String address;
+    private Integer capacity;
+
+    public Location() {
+    }
+
+    public Location(String name, String address, Integer capacity) {
+        this.name = name;
+        this.address = address;
+        this.capacity = capacity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
 }
