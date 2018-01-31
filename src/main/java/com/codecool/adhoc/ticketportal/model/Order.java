6448f05b67d1913ef5db06ed5a45c902ca6aadd7
fruzsinaCsongor <csongor.fruzsina@gmail.com>
2018-01-31 09:44:21 +0100

    Order class with OrderStatus enum class created

--- src/main/java/com/codecool/adhoc/ticketportal/model/Order.java
+++ src/main/java/com/codecool/adhoc/ticketportal/model/Order.java
@@ -1,4 +1,40 @@
 package com.codecool.adhoc.ticketportal.model;
 
+import com.codecool.adhoc.ticketportal.model.enums.OrderStatus;
+
 public class Order {
+
+    private long id;
+
+    private User user;
+
+    private OrderStatus status;
+
+    public Order() {
+    }
+
+    public Order(User user, OrderStatus status) {
+        this.user = user;
+        this.status = status;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
