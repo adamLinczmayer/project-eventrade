c19b893fea98de8f4a33abc3fe0fab2222ecd75a
Sz. N. Bálint <szalaynb93@gmail.com>
2018-01-31 09:35:43 +0100

    LineItem class ready

--- src/main/java/com/codecool/adhoc/ticketportal/model/LineItem.java
+++ src/main/java/com/codecool/adhoc/ticketportal/model/LineItem.java
@@ -1,4 +1,38 @@
 package com.codecool.adhoc.ticketportal.model;
 
 public class LineItem {
+
+    private long id;
+
+    private Ticket ticket;
+
+    private Integer quantity;
+
+    public LineItem() {
+    }
+
+    public LineItem(Ticket ticket, Integer quantity) {
+        this.ticket = ticket;
+        this.quantity = quantity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Ticket getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(Ticket ticket) {
+        this.ticket = ticket;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
