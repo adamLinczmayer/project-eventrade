869d04c8e9af33969a0a61353a6c6a79aef5c6ea
Nopi96912 <nopiwork@gmail.com>
2018-02-15 12:43:44 +0100

    findBandByName

--- src/tests/com/codecool/adhoc/ticketportal/model/BandTest.java
+++ src/tests/com/codecool/adhoc/ticketportal/model/BandTest.java
@@ -18,9 +18,13 @@ class BandTest {
     @BeforeAll
     static void populateDB(){
         Band band = new Band("testBand", MusicStyle.LATIN);
+        Band band1 = new Band("SecondBand", MusicStyle.FOLK);
+        Band band2 = new Band("ThirdBand", MusicStyle.HIPHOP);
         EntityTransaction transaction = em.getTransaction();
         transaction.begin();
         em.persist(band);
+        em.persist(band1);
+        em.persist(band2);
         transaction.commit();
     }
 
@@ -31,8 +35,21 @@ class BandTest {
     }
 
     @Test
-    void findBand() {
+    void testFindBand() {
         Band band1 = em.find(Band.class, 1L);
         assertTrue(band1.getName().equals("testBand"));
+    }
+
+    @Test
+    void testFindAllBand() {
+        assertTrue(em.createNamedQuery("Band.findAllBands", Band.class).getResultList().size()==3);
+    }
+
+    @Test
+    void testFindBandByName() {
+        Band band = em.createNamedQuery("Band.findBandByName", Band.class).setParameter("bandName", "testBand").getSingleResult();
+        Band band1 = em.createNamedQuery("Band.findBandByName", Band.class).setParameter("bandName", "%Second%").getSingleResult();
+        assertTrue(band.getName().equals("testBand"));
+        assertTrue(band1.getName().equals("SecondBand"));
     }
 }
