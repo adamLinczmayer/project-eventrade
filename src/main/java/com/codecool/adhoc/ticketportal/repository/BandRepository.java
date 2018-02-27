571e9785eb1132342edf588fc5188870da76b1a2
fruzsinaCsongor <csongor.fruzsina@gmail.com>
2018-02-27 11:41:37 +0100

    Added FindByMusicStyle query methods

--- src/main/java/com/codecool/adhoc/ticketportal/repository/BandRepository.java
+++ src/main/java/com/codecool/adhoc/ticketportal/repository/BandRepository.java
@@ -1,19 +1,16 @@
 package com.codecool.adhoc.ticketportal.repository;
 
 import com.codecool.adhoc.ticketportal.model.Band;
+import com.codecool.adhoc.ticketportal.model.Event;
+import com.codecool.adhoc.ticketportal.model.enums.MusicStyle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface BandRepository extends JpaRepository<Band, Long>{
-    /*
-    TODO: Band queries
+public interface BandRepository extends JpaRepository<Band, Long> {
 
-    -* findBandById
-    - findAllBands
-    - findBandByName
-    - findBandByMusicStyle
-    -* findBandsByEvent
+    List<Band> findBandsByMusicStyle(MusicStyle musicStyle);
 
-     */
 }
