6eb2bf56e5071f29713c95e2847bee2de04d09d1
Sz. N. Bálint <szalaynb93@gmail.com>
2018-02-27 14:13:30 +0100

    Order Service and Repository are ready

--- src/main/java/com/codecool/adhoc/ticketportal/repository/OrderRepository.java
+++ src/main/java/com/codecool/adhoc/ticketportal/repository/OrderRepository.java
@@ -1,15 +1,22 @@
 package com.codecool.adhoc.ticketportal.repository;
 
 import com.codecool.adhoc.ticketportal.model.Order;
+import com.codecool.adhoc.ticketportal.model.User;
+import com.codecool.adhoc.ticketportal.model.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     /*
     TODO: Order queries
 
-    - createOrder
     - changeOrderStatus
     - findOrdersByUserId
     - findOrdersByStatusAndUserId
@@ -22,5 +29,14 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
      */
 
+    @Transactional
+    @Modifying
+    @Query("UPDATE Order SET status = ?1 WHERE id = ?2")
+    void updateOrderStatus(@Param("status")OrderStatus orderStatus, @Param("orderId")Long orderId);
+
+    List<Order> getOrdersByUser(User user);
+
+    List<Order> getOrdersByUserAndStatus(User user, OrderStatus status);
+    
 
 }
--- src/main/java/com/codecool/adhoc/ticketportal/services/OrderService.java
+++ src/main/java/com/codecool/adhoc/ticketportal/services/OrderService.java
@@ -1,9 +1,14 @@
 package com.codecool.adhoc.ticketportal.services;
 
+import com.codecool.adhoc.ticketportal.model.LineItem;
 import com.codecool.adhoc.ticketportal.model.Order;
+import com.codecool.adhoc.ticketportal.model.User;
+import com.codecool.adhoc.ticketportal.model.enums.OrderStatus;
 import com.codecool.adhoc.ticketportal.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderService {
@@ -14,4 +19,32 @@ public class OrderService {
     public void saveOrder(Order order){
         orderRepository.save(order);
     }
+
+    public void changeOrderStatus(Order order, OrderStatus orderStatus) {
+        orderRepository.updateOrderStatus(orderStatus, order.getId());
+    }
+
+    public List<Order> getOrdersByUser(User user) {
+        return orderRepository.getOrdersByUser(user);
+    }
+
+    public List<Order> getOrdersByUserAndStatus(User user, OrderStatus status) {
+        return orderRepository.getOrdersByUserAndStatus(user, status);
+    }
+
+
+    /*
+    TODO: Order queries
+
+    - changeOrderStatus
+    - findOrdersByUserId
+    - findOrdersByStatusAndUserId
+    - addLineItemToOrder
+    - deleteLineItemFromOrder
+    - clearLineItemsFromOrder
+    - increaseLineItemQuantity
+    - decreaseLineItemQuantity
+    - setLineItemQuantity
+
+     */
 }
