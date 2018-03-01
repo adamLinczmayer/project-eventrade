4534cb48972d33bef3a1e686394223445bc03531
fruzsinaCsongor <csongor.fruzsina@gmail.com>
2018-03-01 10:53:44 +0100

    Added host view's tests

--- src/test/com/codecool/adhoc/ticketportal/controller/ProductControllerTest.java
+++ src/test/com/codecool/adhoc/ticketportal/controller/ProductControllerTest.java
@@ -8,6 +8,7 @@ import com.codecool.adhoc.ticketportal.services.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -18,13 +19,18 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.View;
 
+import java.text.ParseException;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
@@ -51,19 +57,30 @@ class ProductControllerTest {
     @Mock
     private View mockView;
 
+    private Band band1;
+    private Band band2;
+    private Event event1;
+    private Event event2;
+    private Location location1;
+    private Location location2;
+
     @BeforeEach
-    void setUp() {
+    void setUp() throws ParseException {
         MockitoAnnotations.initMocks(this);
         mockMvc = MockMvcBuilders.standaloneSetup(productController)
                 .setSingleView(mockView)
                 .build();
+        band1 = new Band("band1", MusicStyle.JAZZ, "desc");
+        band2 = new Band("band2", MusicStyle.LATIN, "desc");
+        event1 = new Event("event1", new Location(), "2020-10-10-10:10", "desc");
+        event2 = new Event("event2", new Location(), "2019-01-01-01:01", "desc");
+        location1 = new Location("location1", "address", 500);
+        location2 = new Location("location2", "address", 1200);
     }
 
     @Test
     void testRenderEvents() throws Exception {
-        List<Event> expectedEvents = Arrays.asList(
-                new Event("event1", new Location(), "2020-10-10-10:10", "desc"),
-                new Event("event2", new Location(), "2019-01-01-01:01", "desc"));
+        List<Event> expectedEvents = Arrays.asList(event1, event2);
         when(eventService.findUpcomingEvents()).thenReturn(expectedEvents);
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
@@ -78,7 +95,7 @@ class ProductControllerTest {
 
     @Test
     void testRenderSpecificEventPage() throws Exception {
-        Event expectedEvent = new Event("event1", new Location(), "2020-10-10-10:10", "desc");
+        Event expectedEvent = event1;
         when(eventService.findById(1L)).thenReturn(expectedEvent);
         mockMvc.perform(get("/event/{id}", 1))
                 .andExpect(status().isOk())
@@ -87,15 +104,15 @@ class ProductControllerTest {
 
     @Test
     void testRenderSpecificEventPageError() throws Exception {
-        Event expectedEvent = new Event("event1", new Location(), "2020-10-10-10:10", "desc");
+        Event expectedEvent = event1;
         when(eventService.findById(1L)).thenReturn(expectedEvent);
         mockMvc.perform(get("/event/{id}", 100000000))
                 .andExpect(status().isNotFound());
     }
 
     @Test
-    void renderBandPage() throws Exception {
-        Band expectedBand = new Band("band1", MusicStyle.JAZZ, "desc");
+    void testRenderBandPage() throws Exception {
+        Band expectedBand = band1;
         when(bandService.findBandById(1L)).thenReturn(expectedBand);
         mockMvc.perform(get("/band/{id}", 1))
                 .andExpect(status().isOk())
@@ -103,11 +120,72 @@ class ProductControllerTest {
     }
 
     @Test
-    void renderBandPageError() throws Exception {
-        Band expectedBand = new Band("band1", MusicStyle.JAZZ, "desc");
+    void testRenderBandPageError() throws Exception {
+        Band expectedBand = band1;
         when(bandService.findBandById(1L)).thenReturn(expectedBand);
         mockMvc.perform(get("/band/{id}", 1000000000))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testRenderAddEventPage() throws Exception {
+        List<Band> expectedBands = Arrays.asList(band1, band2);
+        List<Location> expectedLocations = Arrays.asList(location1, location2);
+        MusicStyle[] expectedMusicStyles = {MusicStyle.FOLK, MusicStyle.SOUL};
+        when(bandService.findAllBand()).thenReturn(expectedBands);
+        // when(locationService.findAll()).thenReturn(expectedLocations);
+        when(MusicStyle.values()).thenReturn(expectedMusicStyles);
+        mockMvc.perform(get("/addevent", 1))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("allBand", expectedBands))
+                .andExpect(model().attribute("locations", expectedLocations))
+                .andExpect(model().attribute("musicStyles", expectedMusicStyles));
+    }
+
+    @Test
+    void testSaveEvent() throws Exception {
+        Event addedEvent = event2;
+        when(eventService.saveEvent(isA(Event.class))).thenReturn(addedEvent);
+        mockMvc.perform(post("/save-event")
+                .param("event-name", "event2")
+                .param("location", "?")
+                .param("event-date", "2019-01-01")
+                .param("event-time", "01:01")
+                .param("event-description", "desc"))
+                .andExpect(status().isFound())
+                .andExpect(view().name("redirect:/"));
+        ArgumentCaptor<Event> formObjectArgument = ArgumentCaptor.forClass(Event.class);
+        verify(eventService, times(1)).saveEvent(formObjectArgument.capture());
+        verifyNoMoreInteractions(eventService);
+    }
+
+    @Test
+    void testSaveBand() throws Exception {
+        Band addedBand = band2;
+        when(bandService.saveBand(isA(Band.class))).thenReturn(addedBand);
+        mockMvc.perform(post("/add-band")
+                .param("name", "band2")
+                .param("musicStyle", "latin")
+                .param("description", "desc"))
+                .andExpect(status().isOk());
+        ArgumentCaptor<Band> formObjectArgument = ArgumentCaptor.forClass(Band.class);
+        verify(bandService, times(1)).saveBand(formObjectArgument.capture());
+        verifyNoMoreInteractions(bandService);
+    }
+
+    @Test
+    void testSaveLocation() throws Exception {
+        Location addedLocation = location2;
+        when(locationService.saveLocation(isA(Location.class))).thenReturn(addedLocation);
+        mockMvc.perform(post("/add-location")
+                .param("name", "location2")
+                .param("address", "address")
+                .param("capacity", "1200"))
+                .andExpect(status().isOk());
+        ArgumentCaptor<Location> formObjectArgument = ArgumentCaptor.forClass(Location.class);
+        verify(locationService, times(1)).saveLocation(formObjectArgument.capture());
+        verifyNoMoreInteractions(locationService);
+
     }
 
 }
