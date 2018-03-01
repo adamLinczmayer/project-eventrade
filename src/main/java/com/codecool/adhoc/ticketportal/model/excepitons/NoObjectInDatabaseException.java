4f6b11aeb1e5bd3dd6ce046dcbe42d2a1d1be794
Nopi96912 <nopiwork@gmail.com>
2018-03-01 11:30:16 +0100

    NoObjectInDatabaseException annotated

--- src/main/java/com/codecool/adhoc/ticketportal/model/excepitons/NoObjectInDatabaseException.java
+++ src/main/java/com/codecool/adhoc/ticketportal/model/excepitons/NoObjectInDatabaseException.java
@@ -1,5 +1,9 @@
 package com.codecool.adhoc.ticketportal.model.excepitons;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "No data in db")
 public class NoObjectInDatabaseException extends Exception {
     public NoObjectInDatabaseException(String message)
     {
