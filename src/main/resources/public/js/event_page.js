e6b714b8a5cdd2a934417f1fc050f2329d2c836f
Sz. N. Bálint <szalaynb93@gmail.com>
2018-03-01 13:50:30 +0100

    cart_page.html quantity is shown and event_page shows alert if sg added to cart

--- src/main/resources/public/js/event_page.js
+++ src/main/resources/public/js/event_page.js
@@ -11,6 +11,8 @@ $(function() {
             data: details,
             success: function (data) {
                 console.log("add-to-cart success. Data: " + data.responseText);
+                $(".alert").removeClass("in").show();
+                $(".alert").delay(1000).addClass("in").fadeOut(2000);
             },
             error: function (data) {
                 console.log("add-to-cart ERROR. Data: " + data.responseText)
